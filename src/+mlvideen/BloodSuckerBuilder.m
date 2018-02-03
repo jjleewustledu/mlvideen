6bd0915cf08b57a2e334b5b4c5fe1350d85651d1
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2018-02-03 07:08:09 -0600

    Refactoring Herscovitch* classes.

---
+++ src/+mlvideen/BloodSuckerBuilder.m
@@ -0,0 +1,25 @@
+classdef BloodSuckerBuilder < mlpet.IAifDataBuilder
+	%% BLOODSUCKERBUILDER  
+
+	%  $Revision$
+ 	%  was created 12-Dec-2017 16:44:57 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/Local/src/mlcvl/mlvideen/src/+mlvideen.
+ 	%% It was developed on Matlab 9.3.0.713579 (R2017b) for MACI64.  Copyright 2017 John Joowon Lee.
+ 	
+	properties
+ 		
+ 	end
+
+	methods 
+		  
+ 		function this = BloodSuckerBuilder(varargin)
+ 			%% BLOODSUCKERBUILDER
+ 			%  Usage:  this = BloodSuckerBuilder()
+
+ 			
+ 		end
+ 	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
---
+++ src/+mlvideen/TSCBuilder.m
@@ -0,0 +1,25 @@
+classdef TSCBuilder < mlpet.IAifDataBuilder
+	%% TSCBUILDER  
+
+	%  $Revision$
+ 	%  was created 12-Dec-2017 16:32:39 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/Local/src/mlcvl/mlvideen/src/+mlvideen.
+ 	%% It was developed on Matlab 9.3.0.713579 (R2017b) for MACI64.  Copyright 2017 John Joowon Lee.
+ 	
+	properties
+ 		
+ 	end
+
+	methods 
+		  
+ 		function this = TSCBuilder(varargin)
+ 			%% TSCBUILDER
+ 			%  Usage:  this = TSCBuilder()
+
+ 			
+ 		end
+ 	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
